9f6c0053ce4300a12a1ffab8d508bf2b6f236eda
gabriele <gabriele@localhost.localdomain>
2017-09-21 00:01:04 +0200

    primo commit

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+Python 2.7.5 (default, Aug  4 2017, 00:39:18) 
+[GCC 4.8.5 20150623 (Red Hat 4.8.5-16)] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> hw = 'Hello world'
+>>> print hw
+Hello world
+>>> 
